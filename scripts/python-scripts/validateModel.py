866b906b18bdb2a79bf1e0ca581d421d767fba7e
George Sarantinos <georgesarantinos18@gmail.com>
2024-12-30 12:10:58 -0800

    model validate script adjustments

--- scripts/python-scripts/validateModel.py
+++ scripts/python-scripts/validateModel.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO('../../models/best.pt')  # Replace with your model's path
 
 # Run validation
-results = model.val(data='../../resources/fortnite/Datasets/fortnite_elimination_count_logo/data.yaml', imgsz=640, batch=16)
+results = model.val(data='../../resources/fortnite/Datasets/fortnite_elimination_count_logo/data.yaml', imgsz=1920, batch=8)
 
 # Access metrics
 print(results.metrics)  # Includes mAP, Precision, Recall
